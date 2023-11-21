efeb78197e105a922463edbc06e3f831b6dd6597
Jonny Wright <inneon@gmail.com>
2023-11-21 12:14:26 +0000

    fix: double quotes

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   clearMocks: true,
-  coverageProvider: 'v8',
-  modulePathIgnorePatterns: ['lib'],
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/integration/*.spec.ts'],
-  setupFiles: ['./src/tests/jest.overrides.ts'],
-  preset: 'ts-jest',
+  coverageProvider: "v8",
+  modulePathIgnorePatterns: ["lib"],
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/src/integration/*.spec.ts"],
+  setupFiles: ["./src/tests/jest.overrides.ts"],
+  preset: "ts-jest",
 }
